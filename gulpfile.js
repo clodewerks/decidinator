3ba622b6fd3e9c339f73bf2add0a4e36c41b0c90
Kyle Linneman <clodewerks@gmail.com>
2017-05-17 21:47:53 -0700

    Adding Dist gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');    
+var uglify = require('gulp-uglify'); 
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -26,4 +28,33 @@ gulp.task('serve', function() {
       baseDir: 'app'
     },
   })
-})
+});
+
+gulp.task('ship',['ship-html','ship-css','ship-js','ship-fonts','ship-images'],function() {
+
+});
+gulp.task('ship-images', function() {
+  return gulp.src('app/images/*')
+  .pipe(gulp.dest('dist/images'));
+});
+gulp.task('ship-fonts', function() {
+  return gulp.src('app/fonts/*')
+  .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('ship-html', function() {
+  return gulp.src('app/*.html')
+  .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('ship-css', function() {
+  return gulp.src('app/css/**/*.css')
+  .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('ship-js', function() {
+  return gulp.src('app/js/**/*.js')
+  .pipe(concat('scripts.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/js/'));
+});
